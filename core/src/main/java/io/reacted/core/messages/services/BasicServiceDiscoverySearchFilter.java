195cfaf0e153b961e311515d2b8916b1a086e594
wireknight <pierre@reacted.io>
2020-10-11 14:16:29 +0200

    Filter flexibility enhanced

--- core/src/main/java/io/reacted/core/messages/services/BasicServiceDiscoverySearchFilter.java
+++ core/src/main/java/io/reacted/core/messages/services/BasicServiceDiscoverySearchFilter.java
@@ -7,191 +7,22 @@
  */
 
 package io.reacted.core.messages.services;
-
-import com.google.common.collect.Range;
-import io.reacted.core.config.ChannelId;
-import io.reacted.core.config.InheritableBuilder;
-import io.reacted.core.reactorsystem.ReActorRef;
-import io.reacted.core.services.SelectionType;
 import io.reacted.patterns.NonNullByDefault;
-import io.reacted.patterns.Try;
-
-import javax.annotation.Nullable;
 import java.io.Serializable;
-import java.net.InetAddress;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Properties;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 @NonNullByDefault
-public class BasicServiceDiscoverySearchFilter extends InheritableBuilder<BasicServiceDiscoverySearchFilter.Builder,
-                                                                          BasicServiceDiscoverySearchFilter>
+public class BasicServiceDiscoverySearchFilter extends GenericServiceDiscoverySearchFilter<BasicServiceDiscoverySearchFilter.Builder,
+                                                                                           BasicServiceDiscoverySearchFilter>
         implements Serializable, ServiceDiscoverySearchFilter {
 
-    private final String serviceName;
-    private final SelectionType selectionType;
-    @Nullable
-    private final Range<Double> cpuLoad;
-    private final Set<ChannelId> channelIdSet;
-    @Nullable
-    private final InetAddress ipAddress;
-    @Nullable
-    private final Pattern hostName;
-
-    protected BasicServiceDiscoverySearchFilter(Builder builder) {
+    private BasicServiceDiscoverySearchFilter(Builder builder) {
         super(builder);
-        this.serviceName = Objects.requireNonNull(builder.serviceName);
-        this.selectionType = Objects.requireNonNull(builder.selectionType);
-        this.cpuLoad = builder.cpuLoad;
-        this.ipAddress = builder.ipAddress;
-        this.hostName = builder.hostName;
-        this.channelIdSet = Objects.requireNonNull(builder.channelIdSet);
     }
-
-    public String getServiceName() { return this.serviceName; }
-
-    public SelectionType getSelectionType() { return this.selectionType; }
-
-    public Optional<Range<Double>> getCpuLoad() { return Optional.ofNullable(this.cpuLoad); }
-
-    public Set<ChannelId> getChannelIdSet() { return this.channelIdSet; }
-
-    public Optional<InetAddress> getIpAddress() { return Optional.ofNullable(this.ipAddress); }
-
-    public Optional<Pattern> getHostName() { return Optional.ofNullable(this.hostName); }
 
     public static Builder newBuilder() { return new Builder(); }
 
-    @Override
-    public boolean matches(Properties serviceInfos, ReActorRef serviceGate) {
-        return isServiceNameMatching(Objects.requireNonNull(serviceInfos).getProperty(FIELD_NAME_SERVICE_NAME)) &&
-               isCpuLoadMatching((Double)serviceInfos.get(FIELD_NAME_CPU_LOAD)) &&
-               isChannelIdMatching(Objects.requireNonNull(serviceGate)) &&
-               isIpAddressMatching(serviceInfos.getProperty(FIELD_NAME_IP_ADDRESS)) &&
-               isHostNameMatching(serviceInfos.getProperty(FIELD_NAME_HOSTNAME));
-    }
-
-    private boolean isServiceNameMatching(@Nullable String serviceName) {
-        return Objects.equals(getServiceName(), serviceName);
-    }
-
-    private boolean isCpuLoadMatching(@Nullable Double cpuLoad) {
-        return cpuLoad == null ||
-               getCpuLoad().map(reqCpuLoad -> reqCpuLoad.contains(cpuLoad))
-                           .orElse(true);
-    }
-
-    private boolean isChannelIdMatching(ReActorRef serviceGate) {
-        return getChannelIdSet().isEmpty() ||
-               getChannelIdSet().contains(serviceGate.getReActorSystemRef()
-                                                     .getBackingDriver()
-                                                     .getChannelId());
-    }
-
-    private boolean isIpAddressMatching(@Nullable String ipAddress) {
-        return ipAddress == null ||
-               getIpAddress().map(reqIpAddress -> Try.of(() -> InetAddress.getByName(ipAddress))
-                                                     .filter(reqIpAddress::equals)
-                                                     .isSuccess())
-                             .orElse(true);
-    }
-
-    private boolean isHostNameMatching(@Nullable String hostName) {
-        return hostName == null ||
-               getHostName().map(reqHostName -> reqHostName.matcher(hostName).matches())
-                            .orElse(true);
-    }
-
-    @Override
-    public String toString() {
-        return "BasicServiceDiscoverySearchFilter{" + "serviceName='" + serviceName + '\'' + ", selectionType=" +
-               selectionType + ", cpuLoad=" + cpuLoad + ", channelId=" + channelIdSet + ", ipAddress=" + ipAddress +
-               ", hostName=" + hostName + '}';
-    }
-
-    public static class Builder extends InheritableBuilder.Builder<Builder, BasicServiceDiscoverySearchFilter> {
-        @SuppressWarnings("NotNullFieldNotInitialized")
-        private String serviceName;
-        private SelectionType selectionType = SelectionType.ROUTED;
-        @Nullable
-        private Range<Double> cpuLoad;
-        private Set<ChannelId> channelIdSet;
-        @Nullable
-        private InetAddress ipAddress;
-        @Nullable
-        private Pattern hostName;
-
-        protected Builder() { this.channelIdSet = Set.of(); }
-
-        /**
-         *
-         * @param serviceName Name of the service that should be found
-         * @return this builder
-         */
-        public final Builder setServiceName(String serviceName) {
-            this.serviceName = serviceName;
-            return getThis();
-        }
-
-        /**
-         *
-         * @param ipAddress Ip address on which the service should run
-         * @return this builder
-         */
-        public final Builder setIpAddress(@Nullable InetAddress ipAddress) {
-            this.ipAddress = ipAddress;
-            return getThis();
-        }
-
-        /**
-         *
-         * @param hostName regexp defining the hostname that the service should run onto
-         * @return this builder
-         */
-        public final Builder setHostName(@Nullable Pattern hostName) {
-            this.hostName = hostName;
-            return getThis();
-        }
-
-        /**
-         *
-         * @param cpuLoad load range of the host on which the service is running on
-         * @return this builder
-         */
-        public final Builder setCpuLoad(@Nullable Range<Double> cpuLoad) {
-            this.cpuLoad = cpuLoad;
-            return getThis();
-        }
-
-        /**
-         *
-         * @param selectionType Definition on the type of {@link io.reacted.core.services.ReActorService}
-         *                      {@link io.reacted.core.reactorsystem.ReActorRef} required.
-         *                      @see SelectionType
-         * @return this builder
-         */
-        public final Builder setSelectionType(SelectionType selectionType) {
-            this.selectionType = selectionType;
-            return getThis();
-        }
-
-        /**
-         *
-         * @param channelIdSet Definition of the requested {@link io.reacted.core.config.ChannelId} used to
-         *                     communicate with the discovered {@link io.reacted.core.reactorsystem.ReActorRef}
-         * @return this builder
-         */
-        public final Builder setChannelId(Set<ChannelId> channelIdSet) {
-            this.channelIdSet = channelIdSet;
-            return getThis();
-        }
-
-        public final Builder setChannelId(ChannelId channelId) {
-            this.channelIdSet = Set.of(channelId);
-            return getThis();
-        }
+    public static class Builder extends GenericServiceDiscoverySearchFilter.Builder<Builder, BasicServiceDiscoverySearchFilter> {
+        private Builder() { }
 
         @Override
         public BasicServiceDiscoverySearchFilter build() {
