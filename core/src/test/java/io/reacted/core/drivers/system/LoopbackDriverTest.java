56230fa81011b556be3869672e90c329c8760ac0
Razvan Nicoara <nicoara_razvan1987@yahoo.com>
2020-10-18 08:22:22 +0300

    LoopbackDriverTest style fix

--- core/src/test/java/io/reacted/core/drivers/system/LoopbackDriverTest.java
+++ core/src/test/java/io/reacted/core/drivers/system/LoopbackDriverTest.java
@@ -3,7 +3,7 @@
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
  */
 
 package io.reacted.core.drivers.system;
@@ -40,15 +40,15 @@ class LoopbackDriverTest {
     void prepareEnvironment() throws Exception {
         // Prepare & init ReActorSystem
         ReActorSystemConfig reActorSystemConfig = ReActorSystemConfig.newBuilder()
-                                                                     .setReactorSystemName(CoreConstants.REACTED_ACTOR_SYSTEM)
-                                                                     .setMsgFanOutPoolSize(1)
-                                                                     .setLocalDriver(SystemLocalDrivers.DIRECT_COMMUNICATION)
-                                                                     .addDispatcherConfig(DispatcherConfig.newBuilder()
-                                                                                                          .setDispatcherName(testDispatcher)
-                                                                                                          .setBatchSize(1_000)
-                                                                                                          .setDispatcherThreadsNum(1)
-                                                                                                          .build())
-                                                                     .build();
+                .setReactorSystemName(CoreConstants.REACTED_ACTOR_SYSTEM)
+                .setMsgFanOutPoolSize(1)
+                .setLocalDriver(SystemLocalDrivers.DIRECT_COMMUNICATION)
+                .addDispatcherConfig(DispatcherConfig.newBuilder()
+                        .setDispatcherName(testDispatcher)
+                        .setBatchSize(1_000)
+                        .setDispatcherThreadsNum(1)
+                        .build())
+                .build();
         reActorSystem = new ReActorSystem(reActorSystemConfig);
         reActorSystem.initReActorSystem();
 
@@ -56,26 +56,28 @@ class LoopbackDriverTest {
         loopbackDriver.initDriverLoop(reActorSystem);
 
         ReActorConfig reActorConfig = ReActorConfig.newBuilder()
-                                                   .setReActorName("ReActorName")
-                                                   .setDispatcherName(testDispatcher)
-                                                   .setTypedSubscriptions(TypedSubscriptionPolicy.LOCAL.forType(Message.class))
-                                                   .build();
+                .setReActorName("ReActorName")
+                .setDispatcherName(testDispatcher)
+                .setTypedSubscriptions(TypedSubscriptionPolicy.LOCAL.forType(Message.class))
+                .build();
 
         destReActorRef = reActorSystem.spawn(new MagicTestReActor(1, true, reActorConfig))
-                                      .orElseSneakyThrow();
+                .orElseSneakyThrow();
         message = new Message(ReActorRef.NO_REACTOR_REF, destReActorRef, 0, reActorSystem.getLocalReActorSystemId(),
-                              AckingPolicy.NONE, "payload");
+                AckingPolicy.NONE, "payload");
     }
 
     @AfterEach
-    public void cleanup() { reActorSystem.shutDown(); }
+    public void cleanup() {
+        reActorSystem.shutDown();
+    }
 
     @Test
     void loopbackDriverCanTellMessageToReactor() throws ExecutionException, InterruptedException {
         Assertions.assertTrue(loopbackDriver.tell(ReActorRef.NO_REACTOR_REF, destReActorRef, AckingPolicy.NONE, message)
-                                            .toCompletableFuture()
-                                            .get()
-                                            .isSuccess());
+                .toCompletableFuture()
+                .get()
+                .isSuccess());
         // as we have a dispatcher the message was dispatched & it cannot be found in destination mbox
         Awaitility.await().until(() -> MagicTestReActor.RECEIVED.sum() == 1);
     }
