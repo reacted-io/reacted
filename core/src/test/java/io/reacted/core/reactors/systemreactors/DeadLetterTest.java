2b2ecf45d7e7a1529ebf01d7c162f67635414d1f
Nicoara Razvan <nicoara_razvan1987@yahoo.com>
2020-10-18 07:56:42 +0300

    code style in DeadLetterTest

--- core/src/test/java/io/reacted/core/reactors/systemreactors/DeadLetterTest.java
+++ core/src/test/java/io/reacted/core/reactors/systemreactors/DeadLetterTest.java
@@ -28,29 +28,29 @@ class DeadLetterTest {
     void messagesWithUnknownDestinationAreSentToDeadLetter() throws InterruptedException {
         // Prepare & init ReActorSystem
         ReActorSystemConfig reActorSystemConfig = ReActorSystemConfig.newBuilder()
-                                                                     .setReactorSystemName(CoreConstants.REACTED_ACTOR_SYSTEM)
-                                                                     .setMsgFanOutPoolSize(2)
-                                                                     .setLocalDriver(SystemLocalDrivers.DIRECT_COMMUNICATION)
-                                                                     .addDispatcherConfig(DispatcherConfig.newBuilder()
-                                                                                                          .setDispatcherName(CoreConstants.TEST_DISPATCHER)
-                                                                                                          .setBatchSize(1_000)
-                                                                                                          .setDispatcherThreadsNum(1)
-                                                                                                          .build())
-                                                                     .build();
+                .setReactorSystemName(CoreConstants.REACTED_ACTOR_SYSTEM)
+                .setMsgFanOutPoolSize(2)
+                .setLocalDriver(SystemLocalDrivers.DIRECT_COMMUNICATION)
+                .addDispatcherConfig(DispatcherConfig.newBuilder()
+                        .setDispatcherName(CoreConstants.TEST_DISPATCHER)
+                        .setBatchSize(1_000)
+                        .setDispatcherThreadsNum(1)
+                        .build())
+                .build();
         ReActorSystem reActorSystem = new ReActorSystem(reActorSystemConfig);
         reActorSystem.initReActorSystem();
 
         // Spawn new reactor
         ReActorConfig reActorConfig = ReActorConfig.newBuilder()
-                                                   .setReActorName("TR")
-                                                   .setDispatcherName(CoreConstants.TEST_DISPATCHER)
-                                                   .setMailBoxProvider(ctx -> new BasicMbox())
-                                                   .setTypedSubscriptions(TypedSubscriptionPolicy.LOCAL.forType(DeadMessage.class))
-                                                   .build();
+                .setReActorName("TR")
+                .setDispatcherName(CoreConstants.TEST_DISPATCHER)
+                .setMailBoxProvider(ctx -> new BasicMbox())
+                .setTypedSubscriptions(TypedSubscriptionPolicy.LOCAL.forType(DeadMessage.class))
+                .build();
         reActorSystem.spawn(new MagicTestReActor(2, true, reActorConfig))
-                     .orElseSneakyThrow();
+                .orElseSneakyThrow();
         new ReActorRef(new ReActorId(ReActorId.NO_REACTOR_ID, CoreConstants.REACTOR_NAME),
-                       reActorSystem.getLoopback()).tell(ReActorRef.NO_REACTOR_REF, "message");
+                reActorSystem.getLoopback()).tell(ReActorRef.NO_REACTOR_REF, "message");
         Thread.sleep(1000);
         Assertions.assertEquals(1, DeadLetter.RECEIVED.get());
     }
