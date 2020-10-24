edac4c826682b56be9c46c29c1111a0cfe7e5983
wireknight <pierre@reacted.io>
2020-10-24 16:10:27 +0200

    Reactive web service replay example

--- examples/src/main/java/io/reacted/examples/webappbackend/db/DatabaseService.java
+++ examples/src/main/java/io/reacted/examples/webappbackend/db/DatabaseService.java
@@ -7,7 +7,6 @@
  */
 
 package io.reacted.examples.webappbackend.db;
-import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import io.reacted.core.config.reactors.ReActorConfig;
@@ -15,12 +14,9 @@ import io.reacted.core.messages.reactors.ReActorInit;
 import io.reacted.core.reactors.ReActions;
 import io.reacted.core.reactors.ReActor;
 import io.reacted.core.reactorsystem.ReActorContext;
-import io.reacted.core.reactorsystem.ReActorRef;
 import io.reacted.patterns.NonNullByDefault;
 import org.bson.Document;
 import com.mongodb.client.model.Filters;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -33,7 +29,7 @@ import java.util.function.BiConsumer;
 public class DatabaseService implements ReActor {
     private static final String DB_NAME = "reactedapp";
     private static final String COLLECTION = "data";
-    private static final String PAYLOAD_FIELD = "payload";
+    static final String PAYLOAD_FIELD = "payload";
     @Nullable
     private final MongoClient mongoClient;
     @SuppressWarnings("NotNullFieldNotInitialized")
@@ -67,72 +63,13 @@ public class DatabaseService implements ReActor {
     private void onStoreRequest(ReActorContext raCtx, StorageMessages.StoreRequest request) {
         mongoCollection.insertOne(new Document(Map.of("_id", request.getKey(),
                                                       PAYLOAD_FIELD, request.getPayload())))
-                       .subscribe(new MongoStoreSubscriber(raCtx.getSelf(), raCtx.getSender()));
+                       .subscribe(new MongoSubscribers.MongoStoreSubscriber(raCtx.getSelf(), raCtx.getSender()));
     }
 
     private void onQueryRequest(ReActorContext raCtx, StorageMessages.QueryRequest request) {
         mongoCollection.find(Filters.eq("_id", request.getKey()))
-                       .first().subscribe(new MongoQuerySubscriber(raCtx.getSelf(),
-                                                                   raCtx.getSender()));
-    }
-
-    public static class MongoQuerySubscriber implements Subscriber<Document> {
-        @SuppressWarnings("NotNullFieldNotInitialized")
-        private Subscription subscription;
-        private final ReActorRef mongoGate;
-        private final ReActorRef requester;
-        public MongoQuerySubscriber(ReActorRef mongoGate, ReActorRef requester) {
-            this.mongoGate = mongoGate;
-            this.requester = requester;
-        }
-        @Override
-        public void onSubscribe(Subscription subscription) {
-            this.subscription = subscription;
-            subscription.request(1);
-        }
-
-        @Override
-        public void onNext(Document item) {
-            requester.tell(mongoGate, new StorageMessages.QueryReply(item.get(PAYLOAD_FIELD).toString()));
-            this.subscription.cancel();
-        }
-
-        @Override
-        public void onError(Throwable throwable) { }
-
-        @Override
-        public void onComplete() { }
-    }
-
-    private static class MongoStoreSubscriber implements Subscriber<InsertOneResult> {
-        private final ReActorRef requester;
-        private final ReActorRef mongoGate;
-        @SuppressWarnings("NotNullFieldNotInitialized")
-        private Subscription subscription;
-        private MongoStoreSubscriber(ReActorRef mongoGate, ReActorRef requester) {
-            this.requester = requester;
-            this.mongoGate = mongoGate;
-        }
-
-        @Override
-        public void onSubscribe(Subscription subscription) {
-            this.subscription = subscription;
-            subscription.request(1);
-        }
-
-        @Override
-        public void onNext(InsertOneResult item) { }
-
-        @Override
-        public void onError(Throwable throwable) {
-            requester.tell(mongoGate, new StorageMessages.StoreError(throwable));
-        }
-
-        @Override
-        public void onComplete() {
-            requester.tell(mongoGate, new StorageMessages.StoreReply());
-            subscription.cancel();
-        }
+                       .first().subscribe(new MongoSubscribers.MongoQuerySubscriber(raCtx.getSelf(),
+                                                                                    raCtx.getSender()));
     }
 
     private <PayloadT extends Serializable>
---
+++ examples/src/main/java/io/reacted/examples/webappbackend/db/MongoSubscribers.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020 , <Pierre Falda> [ pierre@reacted.io ]
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.reacted.examples.webappbackend.db;
+
+import com.mongodb.client.result.InsertOneResult;
+import io.reacted.core.reactorsystem.ReActorRef;
+import io.reacted.examples.webappbackend.db.DatabaseService;
+import io.reacted.examples.webappbackend.db.StorageMessages;
+import io.reacted.patterns.NonNullByDefault;
+import org.bson.Document;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+@NonNullByDefault
+public final class MongoSubscribers {
+    static class MongoQuerySubscriber implements Subscriber<Document> {
+        @SuppressWarnings("NotNullFieldNotInitialized")
+        private Subscription subscription;
+        private final ReActorRef mongoGate;
+        private final ReActorRef requester;
+
+        MongoQuerySubscriber(ReActorRef mongoGate, ReActorRef requester) {
+            this.mongoGate = mongoGate;
+            this.requester = requester;
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            this.subscription = subscription;
+            subscription.request(1);
+        }
+
+        @Override
+        public void onNext(Document item) {
+            requester.tell(mongoGate, new StorageMessages.QueryReply(item.get(DatabaseService.PAYLOAD_FIELD)
+                                                                         .toString()));
+        }
+
+        @Override
+        public void onError(Throwable throwable) { }
+
+        @Override
+        public void onComplete() { }
+    }
+    static class MongoStoreSubscriber implements Subscriber<InsertOneResult> {
+        private final ReActorRef requester;
+        private final ReActorRef mongoGate;
+        @SuppressWarnings("NotNullFieldNotInitialized")
+        private Subscription subscription;
+        MongoStoreSubscriber(ReActorRef mongoGate, ReActorRef requester) {
+            this.requester = requester;
+            this.mongoGate = mongoGate;
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            this.subscription = subscription;
+            subscription.request(1);
+        }
+
+        @Override
+        public void onNext(InsertOneResult item) { }
+
+        @Override
+        public void onError(Throwable throwable) {
+            requester.tell(mongoGate, new StorageMessages.StoreError(throwable));
+        }
+
+        @Override
+        public void onComplete() {
+            requester.tell(mongoGate, new StorageMessages.StoreReply());
+            subscription.cancel();
+        }
+    }
+}
