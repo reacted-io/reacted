f476e72b52826e70680bdf5371eb767ecdd641b6
wireknight <pierre@reacted.io>
2020-10-31 01:31:38 +0100

    Examples updates

---
+++ examples/src/main/java/io/reacted/examples/quickstart/GreeterService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 , <Pierre Falda> [ pierre@reacted.io ]
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.reacted.examples.quickstart;
+
+import io.reacted.core.config.reactors.ReActorConfig;
+import io.reacted.core.messages.reactors.ReActorInit;
+import io.reacted.core.reactors.ReActions;
+import io.reacted.core.reactors.ReActor;
+
+import javax.annotation.Nonnull;
+import java.io.Serializable;
+
+final class GreeterService implements ReActor {
+
+    @Nonnull
+    @Override
+    public ReActorConfig getConfig() {
+        return ReActorConfig.newBuilder()
+                            .setReActorName("Worker")
+                            .build();
+    }
+
+    @Nonnull
+    @Override
+    public ReActions getReActions() {
+        return ReActions.newBuilder()
+                        .reAct(ReActorInit.class, ((reActorContext, init) -> reActorContext.logInfo("A reactor was " +
+                                                                                                    "born")))
+                        .reAct(GreetingsRequest.class,
+                               (raCtx, greetingsRequest) -> {
+                                       raCtx.logInfo("Replying to {} with {}",
+                                                     raCtx.getSender().getReActorId().getReActorName(),
+                                                     raCtx.getSender().getReActorSystemRef().getBackingDriver().getChannelId().toString());
+                                       raCtx.reply("Hello from " + GreeterService.class.getSimpleName()); } )
+                        .build();
+    }
+    static final class GreetingsRequest implements Serializable { }
+}
---
+++ examples/src/main/java/io/reacted/examples/quickstart/QuickstartServer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020 , <Pierre Falda> [ pierre@reacted.io ]
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.reacted.examples.quickstart;
+
+import io.reacted.core.config.reactorsystem.ReActorSystemConfig;
+import io.reacted.core.drivers.local.SystemLocalDrivers;
+import io.reacted.core.reactorsystem.ReActorSystem;
+import io.reacted.core.reactorsystem.ServiceConfig;
+import io.reacted.drivers.channels.kafka.KafkaDriver;
+import io.reacted.drivers.channels.kafka.KafkaDriverConfig;
+import io.reacted.drivers.serviceregistries.zookeeper.ZooKeeperDriver;
+import io.reacted.drivers.serviceregistries.zookeeper.ZooKeeperDriverConfig;
+import io.reacted.patterns.NonNullByDefault;
+
+@NonNullByDefault
+public class QuickstartServer {
+    public static void main(String[] args) {
+        var zooKeeperDriverConfig = ZooKeeperDriverConfig.newBuilder()
+                                                         .setReActorName("LocalhostCluster")
+                                                         .build();
+        var kafkaDriverConfig = KafkaDriverConfig.newBuilder()
+                                                 .setChannelName("KafkaQuickstartChannel")
+                                                 .setTopic("ReactedTopic")
+                                                 .setGroupId("QuickstartGroupServer")
+                                                 .setMaxPollRecords(1)
+                                                 .setBootstrapEndpoint("localhost:9092")
+                                                 .build();
+        var showOffServerSystem = new ReActorSystem(ReActorSystemConfig.newBuilder()
+                                                                       .setLocalDriver(SystemLocalDrivers.getDirectCommunicationSimplifiedLoggerDriver("/tmp/server"))
+                                                                       .addServiceRegistryDriver(new ZooKeeperDriver(zooKeeperDriverConfig))
+                                                                       .addRemotingDriver(new KafkaDriver(kafkaDriverConfig))
+                                                                       .setReactorSystemName("ShowOffServerActorSystemName")
+                                                                       .build()).initReActorSystem();
+        try {
+            String serviceName = "Greetings";
+            showOffServerSystem.spawnService(ServiceConfig.newBuilder()
+                                                    .setReActorName(serviceName)
+                                                    .setRouteesNum(2)
+                                                    .setRouteeProvider(GreeterService::new)
+                                                    .setIsRemoteService(true)
+                                                    .build())
+                         .orElseSneakyThrow();
+        } catch (Exception anyException) {
+            anyException.printStackTrace();
+            showOffServerSystem.shutDown();
+        }
+    }
+}
