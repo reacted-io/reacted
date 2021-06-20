8fe46dd7a43a02a90d78ff5bdd98466978426f80
wireknight <pierre@reacted.io>
2021-06-20 02:53:28 +0200

    Interface Migration

--- core/src/main/java/io/reacted/core/services/GateSelectorPolicies.java
+++ core/src/main/java/io/reacted/core/services/GateSelectorPolicies.java
@@ -19,10 +19,12 @@ public final class GateSelectorPolicies {
   public static final Function<Collection<ReActorRef>, Optional<ReActorRef>> FIRST_GATE =
       gates -> Optional.ofNullable(gates.isEmpty() ? null : gates.iterator().next());
   public static final Function<Collection<ReActorRef>, Optional<ReActorRef>> RANDOM_GATE =
-      gates -> gates.size() == 1
-               ? Optional.of(gates.iterator().next())
-               : gates.stream()
-                      .skip(ThreadLocalRandom.current().nextInt(0, gates.size() - 1))
-                      .findAny();
+      gates -> gates.isEmpty()
+               ? Optional.empty()
+               : gates.size() == 1
+                 ? Optional.of(gates.iterator().next())
+                 : gates.stream()
+                        .skip(ThreadLocalRandom.current().nextInt(0, gates.size() - 1))
+                        .findAny();
 
 }
