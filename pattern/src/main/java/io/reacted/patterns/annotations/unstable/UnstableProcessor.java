3dbc182a0972e81b5b5ad9b121bd84230e1bb248
wireknight <pierre@reacted.io>
2021-07-10 01:49:02 +0200

    Interface Migration

--- pattern/src/main/java/io/reacted/patterns/annotations/unstable/UnstableProcessor.java
+++ pattern/src/main/java/io/reacted/patterns/annotations/unstable/UnstableProcessor.java
@@ -12,11 +12,13 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 
+@SupportedAnnotationTypes("io.reacted.patterns.annotations.unstable.Unstable")
 @SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class UnstableProcessor extends AbstractProcessor {
 
